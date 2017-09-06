b24b40b9520c3236f8bfe8987c0158bf58328e3b
Maeva TECHER <maeva.techer@oist.jp>
2017-09-06 13:18:06 +0900

    Update freebayes.sh

--- src/freebayes.sh
+++ src/freebayes.sh
@@ -15,5 +15,27 @@
 
 module load freebayes
 
-freebayes --fasta-reference /work/MikheyevU/Maeva/varroahost/ref454/LargeContigs.fasta --use-best-n-alleles 4 --bam /work/MikheyevU/Maeva/varroahost/freebayes/VD149_S1.bam /work/MikheyevU/Maeva/varroahost/freebayes/VD646_1_S17.bam /work/MikheyevU/Maeva/varroahost/freebayes/VJ325_1_S30.bam /work/MikheyevU/Maeva/varroahost/freebayes/VJ950_1_S44.bam -v freebayes4.vcf
+freebayes --fasta-reference /work/MikheyevU/Maeva/varroahost/ref/454LargeContigs.fasta \
+--use-best-n-alleles 4 \
+--bam /work/MikheyevU/Maeva/varroahost/freebayes/VD149_S1.bam \
+/work/MikheyevU/Maeva/varroahost/freebayes/VD212_S6.bam \
+/work/MikheyevU/Maeva/varroahost/freebayes/VD651_1_S18.bam \
+/work/MikheyevU/Maeva/varroahost/freebayes/VD658_2_S21.bam \
+/work/MikheyevU/Maeva/varroahost/freebayes/VD787_2_S3.bam \
+/work/MikheyevU/Maeva/varroahost/freebayes/VD153_2_S3.bam \
+/work/MikheyevU/Maeva/varroahost/freebayes/VD150_2_S6.bam \
+/work/MikheyevU/Maeva/varroahost/freebayes/VD622_1_S12.bam \
+/work/MikheyevU/Maeva/varroahost/freebayes/VD639_1_S14.bam \
+/work/MikheyevU/Maeva/varroahost/freebayes/VD646_1_S17.bam \
+/work/MikheyevU/Maeva/varroahost/freebayes/VJ028_S28.bam \
+/work/MikheyevU/Maeva/varroahost/freebayes/VJ325_1_S30.bam \
+/work/MikheyevU/Maeva/varroahost/freebayes/VJ347_1_S33.bam \
+/work/MikheyevU/Maeva/varroahost/freebayes/VJ853_4_S9.bam \
+/work/MikheyevU/Maeva/varroahost/freebayes/VJ983_1_S51.bam \
+/work/MikheyevU/Maeva/varroahost/freebayes/VJ847_S38.bam  \
+/work/MikheyevU/Maeva/varroahost/freebayes/VJ856_1_S42.bam \
+/work/MikheyevU/Maeva/varroahost/freebayes/VJ952_1_S46.bam \
+/work/MikheyevU/Maeva/varroahost/freebayes/VJ954_2_S48.bam \
+/work/MikheyevU/Maeva/varroahost/freebayes/VJ956_4_S10.bam \
+-v freebayes10.vcf
 
