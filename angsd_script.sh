1b0b21765bcf5f2d7ea2f8946fabf0b0e27ff15d
Maeva TECHER <maeva.techer@oist.jp>
2017-11-01 20:30:21 +0900

    correct pathway ref

--- angsd_script.sh
+++ angsd_script.sh
@@ -18,7 +18,7 @@ ANGSD=/apps/unit/MikheyevU/Maeva/angsd
 SAMTOOLS=/apps/unit/MikheyevU/Maeva/samtools/bin/samtools
 HTSLIB=/apps/unit/MikheyevU/Maeva/htslib
 
-MTDNAREF=/work/MikheyevU/Maeva/varroa-jump/ref/destructor/mtdnamite
+MTDNAREF=/work/MikheyevU/Maeva/varroa-jump/ref/destructor/mtdnamite/VDAJ493124.fasta
 BAMLIST=/work/MikheyevU/Maeva/varroa-jump/data/mtdna_bowtie2/mtbam.list
 OUTDIR=/work/MikheyevU/Maeva/varroa-jump/data/mtdna_var
 
@@ -41,6 +41,6 @@ $ANGSD/angsd -P 12 -b $BAMLIST -ref $MTDNAREF -out $OUTDIR/mite \
         -GL 1 \
         -doMajorMinor 4 \
         -doMaf 1 \
-        -skipTriallelic 1 \
+        -skipTriallelic 1
 
 
