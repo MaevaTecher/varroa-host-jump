46f4a8544f8bb3eda88647381875b4aa48e02ed2
MaevaTecher <maeva-techer@sango10220.oist.jp>
2017-11-02 11:34:35 +0900

    correct angsd script

--- angsd_genome.sh
+++ angsd_genome.sh
@@ -18,30 +18,24 @@ ANGSD=/apps/unit/MikheyevU/Maeva/angsd
 #SAMTOOLS=/apps/unit/MikheyevU/Maeva/samtools/bin/samtools
 #HTSLIB=/apps/unit/MikheyevU/Maeva/htslib
 
-MTDNAREF=/work/MikheyevU/Maeva/varroa-jump/ref/destructor/mtdnamite/VDAJ493124.fasta
-BAMLIST=/work/MikheyevU/Maeva/varroa-jump/data/mtdna_bowtie2/mtbam.list
-OUTDIR=/work/MikheyevU/Maeva/varroa-jump/data/mtdna_var
+VDREF=/work/MikheyevU/Maeva/varroa-jump/ref/destructor/vd.fasta
+BAMLIST=/work/MikheyevU/Maeva/varroa-jump/data/angsd/ngmbam.list
+OUTDIR=/work/MikheyevU/Maeva/varroa-jump/data/angsd
 
-$ANGSD/angsd -P 12 -b $BAMLIST -ref $MTDNAREF -out $OUTDIR/mite \
+$ANGSD/angsd -P 12 -b $BAMLIST -ref $VDREF -out $OUTDIR/mitegeno \
         -uniqueOnly 1 \
         -remove_bads 1 \
         -only_proper_pairs 1\
-        -SNP_pval 1e-8\
-        -minMapQ 20 \
-        -minQ 20 \
-	-doGlf 2 \
-	-doGeno 32\
-        -trim 0 \
-        -C 50 \
-        -baq 1 \
-	#-minInd 15 \
-        #-setMinDepth 60 \
-        #-setMaxDepth 400 \
-        -doCounts 1 \
-        -GL 1 \
-        -doMajorMinor 4 \
-        -doMaf 1 \
-        -skipTriallelic 1\
-	-doPost 1
-
-
+	-trim 0 \
+	-C 50\
+	-baq 1\	
+	-minMapQ 20
+	-minQ 20
+	-doCounts 1\
+	-GL 1\
+	-doMajorMinor 1\
+	-doMaf 1\
+	-skipTriallelic 1\
+	-SNP_pval 1e-8
+      	-doGeno 32\
+        -doPost 1
